a5b44c8119f2378e79a7afa2bdf1cb5363d0ef34
Нечепоренко Степан <neceporenkostepan@gmail.com>
2022-10-28 11:34:46 +0300

    add messages aggregation

--- monitor/Notifier.py
+++ monitor/Notifier.py
@@ -5,7 +5,7 @@ from .AsyncClient import AsyncClient
 
 
 
-@dataclasses.dataclass(frozen=True)
+@dataclasses.dataclass
 class Notifier:
 
 	token: bytes
@@ -13,32 +13,56 @@ class Notifier:
 
 	client: AsyncClient
 
+	_last_error_result: dict[str, list | int | Exception] = dataclasses.field(default_factory=dict)
+
 	async def __call__(self, url: str, result: list | int | Exception) -> None:
 
 		if type(result) == list:
-			return
 
-		message_additional_args = {}
-		if type(result) == int:
-			message_additional_args = {
-				'BadReply': {
-					'status_code': str(result).encode()
+			if url in self._last_error_result:
+
+				del self._last_error_result[url]
+
+				response = await self.client.get(
+					Template('RequestUrl')({
+						'token': self.token,
+						'chat_id': self.chat_id,
+						'MessageOk': {
+							'url': url.encode()
+						}
+					}).decode()
+				)
+				print(f'{url} is OK -> {response}')
+
+		else:
+
+			if url in self._last_error_result:
+				if self._last_error_result[url] == result:
+					return
+
+			self._last_error_result[url] = result
+
+			message_additional_args = {}
+			if type(result) == int:
+				message_additional_args = {
+					'BadReply': {
+						'status_code': str(result).encode()
+					}
 				}
-			}
-		elif type(result) == Exception:
-			message_additional_args = {
-				'Exception': {
-					'text': str(result).encode()
+			elif type(result) == Exception:
+				message_additional_args = {
+					'Exception': {
+						'text': str(result).encode()
+					}
 				}
-			}
 
-		request_url = Template('RequestUrl')({
-			'token': self.token,
-			'chat_id': self.chat_id,
-			'Message': {
-				'url': url.encode(),
-			} | message_additional_args
-		}).decode()
-
-		response = await self.client.get(request_url)
-		print(f'{request_url} -> {response}')
+			response = await self.client.get(
+				Template('RequestUrl')({
+					'token': self.token,
+					'chat_id': self.chat_id,
+					'MessageError': {
+						'url': url.encode(),
+					} | message_additional_args
+				}).decode()
+			)
+			print(f'{url} is NOT OK -> {response}')
