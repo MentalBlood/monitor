eacffd5f304be5bfb37b15b4632863628b1eea61
Нечепоренко Степан <neceporenkostepan@gmail.com>
2022-11-02 12:07:02 +0300

    accepting raw proxy address, no modifying

--- monitor/AsyncClient.py
+++ monitor/AsyncClient.py
@@ -7,7 +7,7 @@ class AsyncClient(httpx.AsyncClient):
 
 	@cache
 	def __new__(cls, proxy: str) -> httpx.AsyncClient:
-		return httpx.AsyncClient(proxies=f'http://{proxy}')
+		return httpx.AsyncClient(proxies=proxy)
 
 	def __init__(self, proxy: str):
 		pass
